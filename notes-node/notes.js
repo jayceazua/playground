10a8650366b8510979ba50b744aee2af49d5406e
Jayce Azua <jayce.azua@gmail.com>
2018-12-27 21:56:09 -0500

    addNote function

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,18 +1,36 @@
 console.log('Starting notes.js');
 
-let addNote = (title, body) => {
-  console.log("Adding note:", title, body);
+const fs = require('fs');
+
+var addNote = (title, body) => {
+  let notes = [];
+  let note = {
+    title,
+    body
+  };
+
+  try {
+    let notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString);
+  } catch (err) {};
+
+  let duplicateNotes = notes.filter((note) => note.title === title);
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  };
+
 };
 
-let getAll = () => {
+function getAll() {
   console.log('Getting all notes');
 };
 
-let getNote = (title) => {
+function getNote(title) {
   console.log('Reading note', title);
 };
 
-let removeNote = (title) => {
+function removeNote(title) {
   console.log('Removing note', title);
 };
 
